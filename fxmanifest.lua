d7df68eab0f3f89bcba199afb806aa076d9f8212
Lorenzo Rizzotti <10401566+Dreaming-Codes@users.noreply.github.com>
2021-04-25 17:15:27 +0200

    autocompile

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,10 +5,10 @@ name 'fatpeople'
 author 'DreamingCodes'
 url 'https://dreaming.codes'
 
---dependency 'yarn'
---dependency 'webpack'
+dependency 'yarn'
+dependency 'webpack'
 
---webpack_config 'webpack.config.js'
+webpack_config 'webpack.config.js'
 
 client_script 'dist/client/*.client.js'
 
